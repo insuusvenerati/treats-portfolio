976f8563991e5be81615ac5be7fc1ecfc47cf015
Sean Norwood <norwood.sean@gmail.com>
2020-03-01 16:19:06 -0600

    refactor(cards): removes title and description from cards

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Creative Portfolio`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,8 +11,8 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: process.env.DATO_API_TOKEN,
-      },
-    },
-  ],
+        apiToken: process.env.DATO_API_TOKEN
+      }
+    }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Masonry from 'react-masonry-component'
-import Img from 'gatsby-image'
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Masonry from "react-masonry-component"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => (
@@ -13,14 +13,6 @@ const IndexPage = ({ data }) => (
             <Link to={`/works/${work.slug}`} className="card__image">
               <Img fluid={work.coverImage.fluid} />
             </Link>
-            <figcaption className="card__caption">
-              <h6 className="card__title">
-                <Link to={`/works/${work.slug}`}>{work.title}</Link>
-              </h6>
-              <div className="card__description">
-                <p>{work.excerpt}</p>
-              </div>
-            </figcaption>
           </figure>
         </div>
       ))}
@@ -40,7 +32,7 @@ export const query = graphql`
           slug
           excerpt
           coverImage {
-            fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
+            fluid(maxWidth: 300, imgixParams: { fm: "jpg", auto: "compress" }) {
               ...GatsbyDatoCmsSizes
             }
           }
