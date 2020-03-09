532dbb76e473d436a96becbe68ead8e0e32a0c69
Sean Norwood <norwood.sean@gmail.com>
2020-03-09 13:39:41 -0500

    feat(errors): adds sentry for error checking and errorboundry

---
+++ src/components/errorboundry.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import * as Sentry from '@sentry/browser';
+
+export default class ErrorBoundry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+    Sentry.configureScope((scope) => {
+      Object.keys(errorInfo).forEach((key) => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+    });
+    Sentry.captureException(error);
+  }
+
+  render() {
+    const { error } = this.state;
+    const { children } = this.props;
+    if (error) {
+      return <h1>Something went wrong</h1>;
+    }
+    return children;
+  }
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 import DatoContainer from '../components/datocontainer';
+import ErrorBoundry from '../components/errorboundry';
 
-const IndexPage = () => {
-  return (
+const IndexPage = () => (
+  <ErrorBoundry>
     <Layout>
       <DatoContainer />
     </Layout>
-  );
-};
+  </ErrorBoundry>
+);
 
 export default IndexPage;
