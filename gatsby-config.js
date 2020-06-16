699be2bee666bff36e0e38daa0fcd8a7ca848d14
Sean Norwood <norwood.sean@gmail.com>
2020-06-16 13:59:36 -0500

    chore: update deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,24 @@ require('dotenv').config({
 const plugins = [
   'gatsby-plugin-react-helmet',
   'gatsby-plugin-sass',
-  'gatsby-plugin-manifest',
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: "Laura Norwood",
+      short_name: "Laura Norwood",
+      start_url: "/",
+      background_color: "#F0E6E0",
+      theme_color: "#9DAD9F",
+      // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+      // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+      display: "standalone",
+      // icon: "src/images/icon.png", // This path is relative to the root of the site.
+      // An optional attribute which provides support for CORS check.
+      // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+      // Any invalid keyword or empty string defaults to `anonymous`
+      crossOrigin: ``,
+    },
+  },
   `gatsby-plugin-postcss`,
   `gatsby-transformer-remark`,
   `gatsby-plugin-webpack-size`,
