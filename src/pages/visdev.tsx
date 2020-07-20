2511e16632a2d8323e852100d8ca538fddc346ca
Sean Norwood <norwood.sean@gmail.com>
2020-07-20 18:53:34 -0500

    chore: add renovate and travis
    Signed-off-by: Sean Norwood <norwood.sean@gmail.com>

--- src/pages/visdev.tsx
+++ src/pages/visdev.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {PageProps} from 'gatsby';
+import { PageProps } from 'gatsby';
 import Layout from '../components/layout';
 import VisdevImageContainer from '../components/visdevImageContainer';
 
