c6ea21d960a5eac694fb885ca1d45ff75eb3db37
Sean Norwood <norwood.sean@gmail.com>
2020-07-23 18:24:18 -0500

    feature: randomly generate cat images

--- src/components/catGenerator.tsx
+++ src/components/catGenerator.tsx
@@ -1,39 +1,8 @@
-import { graphql, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
+import Img, { FixedObject } from 'gatsby-image';
 import React from 'react';
 
-type DatoCmsAssetNodes = {
-  fixed: {
-    src: string;
-    base64: string;
-    tracedSVG: string;
-    aspectRatio: number;
-    width: number;
-    height: number;
-    srcSet: string;
-    sizes: string;
-  };
-};
-
-type CatImages = {
-  allDatoCmsAsset: {
-    nodes: Array<DatoCmsAssetNodes>;
-  };
-};
-
-const CatGenerator: React.FC = () => {
-  const { allDatoCmsAsset } = useStaticQuery<CatImages>(graphql`
-    query CatQuery {
-      allDatoCmsAsset(filter: { tags: { eq: "cats" } }) {
-        nodes {
-          fixed(width: 149) {
-            ...GatsbyDatoCmsFixed
-          }
-        }
-      }
-    }
-  `);
-  return <Img style={{ borderRadius: '7px', zIndex: 2 }} fixed={allDatoCmsAsset.nodes[0].fixed} />;
+const CatGenerator: React.FC<{ catImageFixed: FixedObject }> = ({ catImageFixed }) => {
+  return <Img style={{ borderRadius: '7px', zIndex: 2 }} fixed={catImageFixed} />;
 };
 
 export default CatGenerator;
