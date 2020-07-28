9ce999fba43cf95490c40e22adba08a8e53603e7
Sean Norwood <norwood.sean@gmail.com>
2020-07-28 14:28:48 -0500

    chore: remove codegen from production builds

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ const plugins = [
   'gatsby-plugin-react-helmet',
   `gatsby-plugin-robots-txt`,
   `gatsby-plugin-sitemap`,
-  `gatsby-plugin-codegen`,
   {
     resolve: `gatsby-theme-contact`,
     options: {
@@ -78,6 +77,10 @@ const plugins = [
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push('gatsby-plugin-preact');
+}
+
+if (process.env.NODE_ENV === 'development') {
+  plugins.push(`gatsby-plugin-codegen`);
 }
 
 module.exports = {
