861122c7e7bb82f210cac8c665b94483e6fb4de9
Sean Norwood <norwood.sean@gmail.com>
2020-07-28 16:31:32 -0500

    refactor: update success message

---
+++ src/gatsby-theme-contact/components/success-message.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuccessMessage: React.FC = () => (
+  <>
+    <h2>
+      Message received <span>🐸</span>
+    </h2>
+  </>
+);
+
+export default SuccessMessage;
