0862c0c944cafd7fecf6191e9a7654628db3a4a2
Sean Norwood <norwood.sean@gmail.com>
2020-08-05 18:41:16 -0500

    style: update contact page for mobile

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,11 @@
 import './src/styles/index.sass';
 import 'react-awesome-button/dist/styles.css';
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` + `Reload to display the latest version?`,
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
