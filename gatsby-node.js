7d13d4e5a815119f01770dc99393f5eb3969657f
Sean Norwood <norwood.sean@gmail.com>
2020-09-25 15:55:59 -0500

    chore: require dotenv

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,13 @@
 /* eslint-disable */
+require('dotenv').config({
+  path: '.env',
+});
+
 exports.sourceNodes = async ({ actions, createContentDigest }) => {
   const { createNode } = actions;
 
   const data = await fetch(
-    `https://api.gumroad.com/v2/products?access_token=${process.env.GATSBY_GUMROAD_ACCESS_TOKEN}`,
+    `https://api.gumroad.com/v2/products?access_token=${process.env.GUMROAD_ACCESS_TOKEN}`,
   ).then((res) => res.json());
 
   data.products.map((product) => {
