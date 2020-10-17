32d14e46730f1c9a20b93235cfc226cc347b3ab4
Sean Norwood <norwood.sean@gmail.com>
2020-10-17 17:03:15 -0500

    chore: switch to using WSL

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import Img, { FluidObject } from 'gatsby-image';
+import React from 'react';
 import {
   BGTags_desktopBgImage_edges,
   BGTags_desktopBgImage_edges_node,
