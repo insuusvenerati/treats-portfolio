5cf29aefcc37e8fab8b37597998b40ae4b9a0248
Sean Norwood <norwood.sean@gmail.com>
2021-03-06 19:58:45 -0600

    ci: semantic release @git plugin

--- release.config.js
+++ release.config.js
@@ -14,10 +14,12 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
+    '@semantic-release/npm',
     [
-      '@semantic-release/npm',
+      '@semantic-release/git',
       {
-        npmPublish: false,
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [CI SKIP]\n\n${nextRelease.notes}',
       },
     ],
   ],
