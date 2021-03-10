4afdab3649858818e0da24538317369c111a3384
Sean Norwood <norwood.sean@gmail.com>
2021-03-09 23:23:23 -0600

    ci: disable lhci assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,9 +5,9 @@ module.exports = {
       url: ['http://localhost/', 'http://localhost/about'],
       isSinglePageApplication: true,
     },
-    assert: {
-      preset: 'lighthouse:recommended',
-    },
+    // assert: {
+    //   preset: 'lighthouse:recommended',
+    // },
     upload: {
       target: 'lhci',
       serverBaseUrl: 'https://stiforr-lighthouse.herokuapp.com/',
